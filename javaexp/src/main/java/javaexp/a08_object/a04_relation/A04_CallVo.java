ef4a19c5331c507737e6913f41331fca024a0820
USER <USER@DESKTOP-DBJQEKM>
2025-05-01 15:34:03 +0900

    111

--- javaexp/src/main/java/javaexp/a08_object/a04_relation/A04_CallVo.java
+++ javaexp/src/main/java/javaexp/a08_object/a04_relation/A04_CallVo.java
@@ -1,6 +1,7 @@
 package javaexp.a08_object.a04_relation;
 
 import javaexp.a08_object.a04_relation.vo.Dept;
+import javaexp.a08_object.a04_relation.vo.Person;
 
 public class A04_CallVo {
 
@@ -14,6 +15,14 @@ public class A04_CallVo {
 		System.out.println(d1.getDname());
 		// ex) javaexp.a08_object.a04_relation.vo 에  Person  name, age, loc
 		//  해당 객체를 호출하여, 생성자를 통해 초기화한 후, get/set메서드로 가져오거나 변경하세요..
+		Person p01 = new Person("홍길동",25,"서울");
+		System.out.println(p01.getName());
+		System.out.println(p01.getAge());
+		System.out.println(p01.getLoc());
+		p01.setAge(30);
+		System.out.println(p01.getAge());
+		
+		
 		
 	}
 
