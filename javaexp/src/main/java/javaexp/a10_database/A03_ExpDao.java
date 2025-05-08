c8a6d56db9e8d5fa4176d8bde5b7d64c89c8e464
USER <USER@DESKTOP-DBJQEKM>
2025-05-08 12:53:55 +0900

    11

---
+++ javaexp/src/main/java/javaexp/a10_database/A03_ExpDao.java
@@ -0,0 +1,44 @@
+package javaexp.a10_database;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javaexp.a10_database.dto.Emp;
+
+
+public class A03_ExpDao {	
+	// SELECT 템플릿
+	public List<Emp> getSelectEmp(Emp sch){
+		List<Emp> list  = new ArrayList<Emp>();
+		String sql = "SELECT * FROM EMP10";
+		try( Connection con = DB.con();
+			 PreparedStatement pstmt = con.prepareStatement(sql);			 
+				){
+				pstmt.setString(1, sch.getEname());
+			try(ResultSet rs = pstmt.executeQuery()){
+				while(rs.next()) {   
+					list.add(new Emp(rs.getInt("EMPNO"),rs.getString("ENAME"),rs.getString("JOB"),rs.getInt("MGR"),
+									rs.getDate("HIREDATE"),rs.getDouble("SAL"),rs.getDouble("COMM"),rs.getInt("DEPTNO") ));
+				}
+				System.out.println("데이터 로딩 완료:"+list.size());				
+			}
+		}catch(SQLException e) {
+			System.out.println("DB처리 에러:"+e.getMessage());
+		}catch(Exception e) {
+			System.out.println("기타 에러:"+e.getMessage());
+		}
+		return list;
+	}
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- javaexp/src/main/webapp/c01_database/a01_EmpListAll.jsp
+++ javaexp/src/main/webapp/c01_database/a01_EmpListAll.jsp
@@ -1,6 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"
     import="javaexp.a10_database.A02_EmpDao"
+    import="javaexp.a10_database.dto.Emp"
     %>
 <!DOCTYPE html>
 <html>
@@ -10,6 +11,23 @@
 <link rel="stylesheet" href="/javaexp/a00_com/com.css">
 </head>
 <body>
-	<h2>사원정보리스트</h2>
+	<h2 class="title">사원정보리스트</h2>
+
+	<%
+	// java 코드가 삽입되는 부분..
+	A02_EmpDao dao = new A02_EmpDao();
+	%>
+	<table class="data-table">
+		<tr><th>사원번호</th><th>사원명</th><th>급여</th><th>부서번호</th></tr>
+		<% 
+		for(Emp e:dao.getEmpAll()){
+		%>
+		<tr><td><%=e.getEmpno()%></td><td><%=e.getEname() %></td>
+			<td><%=e.getSal() %></td><td><%=e.getDeptno() %></td></tr>
+		<% 
+		}
+		%>
+		
+	</table>	
 </body>
 </html>
