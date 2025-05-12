ab8667433fe40aa0d2469cc00575de57ae0e7b92
USER <USER@DESKTOP-DBJQEKM>
2025-05-12 11:40:02 +0900

    1111

--- javaexp/src/main/java/javaexp/a10_database/A04_ExpDao.java
+++ javaexp/src/main/java/javaexp/a10_database/A04_ExpDao.java
@@ -61,21 +61,21 @@ public class A04_ExpDao {
 		int empno = 0;
 		String sql = "SELECT EMPNO FROM EMP WHERE ENAME = ?";	
 		try( Connection con = DB.con();
-				PreparedStatement pstmt = con.prepareStatement(sql); ){
-				pstmt.setString(1, ename);  // set문자열(1번째, ename)
-				try(ResultSet rs = pstmt.executeQuery()){
-					if(rs.next()) {  // 단일행 
-						empno = rs.getInt("EMPNO"); // get결과유형("컬럼명")
-						// 컬럼과 데이터 유형에 따라  rs.get데이터유형("컬럼명");
-						// 리턴할 결과값 sal에 할당처리
-					}
-					System.out.println("데이터 로딩 완료:");				
+			PreparedStatement pstmt = con.prepareStatement(sql); ){
+			pstmt.setString(1, ename);  // set문자열(1번째, ename)
+			try(ResultSet rs = pstmt.executeQuery()){
+				if(rs.next()) {  // 단일행 
+					empno = rs.getInt("EMPNO"); // get결과유형("컬럼명")
+					// 컬럼과 데이터 유형에 따라  rs.get데이터유형("컬럼명");
+					// 리턴할 결과값 sal에 할당처리
 				}
-			}catch(SQLException e) {
-				System.out.println("DB처리 에러:"+e.getMessage());
-			}catch(Exception e) {
-				System.out.println("기타 에러:"+e.getMessage());
-			}		
+				System.out.println("데이터 로딩 완료:");				
+			}
+		}catch(SQLException e) {
+			System.out.println("DB처리 에러:"+e.getMessage());
+		}catch(Exception e) {
+			System.out.println("기타 에러:"+e.getMessage());
+		}		
 		return empno;
 	}
 	
@@ -85,6 +85,22 @@ public class A04_ExpDao {
 		String job = "";
 		String sql = "SELECT JOB FROM EMP WHERE SAL = ?";
 		
+		try( Connection con = DB.con();
+			PreparedStatement pstmt = con.prepareStatement(sql); ){
+			pstmt.setDouble(1, sal);  // set문자열(1번째, ename)
+			try(ResultSet rs = pstmt.executeQuery()){
+				if(rs.next()) {  // 단일행 
+					job = rs.getString("JOB"); // get결과유형("컬럼명")
+					// 컬럼과 데이터 유형에 따라  rs.get데이터유형("컬럼명");
+					// 리턴할 결과값 sal에 할당처리
+				}
+				System.out.println("데이터 로딩 완료:");				
+			}
+		}catch(SQLException e) {
+			System.out.println("DB처리 에러:"+e.getMessage());
+		}catch(Exception e) {
+			System.out.println("기타 에러:"+e.getMessage());
+		}				
 		
 		return job;
 	}	
