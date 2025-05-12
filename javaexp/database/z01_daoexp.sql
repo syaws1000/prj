ef95790319a98499ebf0092fe248deb8e984d14f
USER <USER@DESKTOP-DBJQEKM>
2025-05-12 11:00:57 +0900

    111

--- javaexp/database/z01_daoexp.sql
+++ javaexp/database/z01_daoexp.sql
@@ -18,13 +18,50 @@ public int getCountEmp(int deptno){
 
 -- 4. try catch문 복사
 -- 5. pstmt.setXXX 설정
+--    매개변수로 데이터를 ?에 연결 처리..	
 -- 6. rs.next(), rs.getXXX()
 
 
 -- sql을 통한 메서드 선언 연습문제
 -- 1. 사원번호를 통하여 급여을 처리하는 sql과 메서드를 선언하세요..
+SELECT SAL FROM EMP WHERE EMPNO = 7369;
+/*
+입력 int empno, 결과값 double sal;
+public double getSalByEmpno(int empno){
+	double sal = 0;
+	String sql = "SELECT SAL FROM EMP WHERE EMPNO = ?";
+	
+	return sal;
+}
+*/
 -- 2. 사원명을 통하여 사원번호를 처리하는 sql과 메서드를 선언하세요.
--- 3. 
+SELECT EMPNO FROM EMP WHERE ENAME = 'SMITH';
+/*
+입력 : String name;
+결과 : int empno
+public int getEmpnoByEname(String ename){
+	int empno = 0;
+	String sql = "SELECT EMPNO FROM EMP WHERE ENAME = ?";	
+	return empno;
+}
+
+*/
+-- 3. 급여를 통하여 직책을 처리하는 sql과 메서드를 선언하세요.
+SELECT * FROM EMP;
+SELECT JOB FROM EMP WHERE SAL = 800;
+/*
+입력 : double sal
+결과 : String job
+public String getJobBySal(double sal){
+	String job = "";
+	String sql = "SELECT JOB FROM EMP WHERE SAL = ?";
+	
+	
+	return job;
+}
+
+*/
+
 SELECT * FROM EMP;
 
 
--- javaexp/src/main/java/javaexp/a10_database/A04_ExpDao.java
+++ javaexp/src/main/java/javaexp/a10_database/A04_ExpDao.java
@@ -1,10 +1,33 @@
 package javaexp.a10_database;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javaexp.a10_database.dto.Emp;
+
 public class A04_ExpDao {
 
 	public int getCountEmp(int deptno){
 		int count = 0;
 		String sql = "SELECT COUNT(*) CNT FROM EMP WHERE DEPTNO=?";
+		try( Connection con = DB.con();
+			PreparedStatement pstmt = con.prepareStatement(sql); ){
+			pstmt.setInt(1, deptno);
+			try(ResultSet rs = pstmt.executeQuery()){
+				if(rs.next()) {  // 단일행 
+					count = rs.getInt("CNT");
+					// 컬럼과 데이터 유형에 따라  rs.get데이터유형("컬럼명");
+					// 리턴할 결과값 count에 할당처리
+				}
+				System.out.println("데이터 로딩 완료:");				
+			}
+		}catch(SQLException e) {
+			System.out.println("DB처리 에러:"+e.getMessage());
+		}catch(Exception e) {
+			System.out.println("기타 에러:"+e.getMessage());
+		}
 		
 		return count;
 	} 	
