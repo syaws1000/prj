6f3dd375f38861246182d175f9015c2d14dc7bbe
USER <USER@DESKTOP-DBJQEKM>
2025-05-14 10:37:46 +0900

    111

--- javaexp/src/main/java/javaexp/a10_database/A01_Basic.java
+++ javaexp/src/main/java/javaexp/a10_database/A01_Basic.java
@@ -2,9 +2,47 @@ package javaexp.a10_database;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javaexp.a10_database.dto.Emp;
 
 public class A01_Basic {
+	public List<Emp> getTmpList(Emp sch){ // new Emp("직책", 10)
+		List<Emp> list = new ArrayList<Emp>();
+		String sql = "SELECT * FROM EMP WHERE DEPTNO = ? AND JOB  = ?";
+
+		try( Connection con = DB.con();
+			 PreparedStatement pstmt = con.prepareStatement(sql); ){
+			pstmt.setInt(1, sch.getDeptno());
+			pstmt.setString(2, sch.getJob());
+			try(ResultSet rs = pstmt.executeQuery()){
+				while(rs.next()) {  // 여러행 
+					list.add(new Emp(
+								rs.getInt("EMPNO"),
+								rs.getString("ENAME"),
+								rs.getString("JOB"),
+								rs.getInt("MGR"),
+								rs.getDate("HIREDATE"),
+								rs.getDouble("SAL"),
+								rs.getDouble("COMM"),
+								rs.getInt("DEPTNO")
+							 )
+							);
+				}
+				System.out.println("데이터 로딩 완료:"+list.size());				
+			}
+		}catch(SQLException e) {
+			System.out.println("DB처리 에러:"+e.getMessage());
+		}catch(Exception e) {
+			System.out.println("기타 에러:"+e.getMessage());
+		}		
+		return list;
+	}	
+	
 	// DB 연결 처리하는 모듈만들기..
 	
 	// 1. 연결 처리하는 메서드.
