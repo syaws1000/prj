754f2563bcb3ccf03d16333e19de03c7da3411ec
USER <USER@DESKTOP-DBJQEKM>
2025-05-14 09:50:59 +0900

    111

--- javaexp/src/main/java/javaexp/a10_database/dto/Order.java
+++ javaexp/src/main/java/javaexp/a10_database/dto/Order.java
@@ -9,16 +9,35 @@ public class Order {
 	private int orderId;
 	private Date orderDate;
 	private int orderAmount;
+	// 클래스 != 객체   클래스를 통해 다양한 객체(필드값을 초기화)를 생성할 수 있다. 
+	// 클래스 객체의 생성을 위한 도면, 실제 객체가 아니다....  
+	// 클래스틑 객체의 생성 하기 위한 틀(붕어빵틀), 객체는 틀에서 나온 실체(붕어빵)
+	// Order o = new Order();  // 초기값이 생성되지 않는 생성..
 	public Order() {
 		// TODO Auto-generated constructor stub
 	}
+	// sql mapping을 위한 생성자 선언과 객체 생성 
+	// Order o1 = new Order("해리포터", 1000,2000);  // 이름, 시작, 마지막  : SQL의 ?? 입력값 설정의 위한 생성
+	// sql = "SELECT * FROM ORDER WHERE WIZARD_NAME LIKE ? AND ORDER_AMOUNT BETWEEN ? AND ?
 	public Order(String wizardName, int frOrder, int toOrder) {
 		this.wizardName = wizardName;
 		this.frOrder = frOrder;
 		this.toOrder = toOrder;
 	}
+	// 데이터 결과값을 저장하기 위한 생성자 선언과 객체 생성..
+	// Order o2 = new Order(rs.getString("WIZARD_NAME"), rs.getInt("ORDER_ID"), 
+	//                       rs.getDate("ORDER_DATE"), rs.getInt("ORDER_AMOUNT"));
 	public Order(String wizardName, int orderId, Date orderDate, int orderAmount) {
 		this.wizardName = wizardName;
+		this.orderId = orderId;
+		this.orderDate = orderDate;
+		this.orderAmount = orderAmount;
+	}
+	
+	public Order(String wizardName, int frOrder, int toOrder, int orderId, Date orderDate, int orderAmount) {
+		this.wizardName = wizardName;
+		this.frOrder = frOrder;
+		this.toOrder = toOrder;
 		this.orderId = orderId;
 		this.orderDate = orderDate;
 		this.orderAmount = orderAmount;
