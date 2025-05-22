845d3c79e03671a879ac1c151816385506a15a77
양현수 <zelratole@gmail.com>
2025-05-22 15:14:09 +0900

    111

--- jspexp/src/main/java/jspexp/a10_database/A02_EmpDao.java
+++ jspexp/src/main/java/jspexp/a10_database/A02_EmpDao.java
@@ -107,6 +107,41 @@ public class A02_EmpDao {
 		return list;
 	}
 
+	// a08_deptList.jsp ==> 부서정보를 테이블에 리스트하는 내용을 만들어 주세요..
+	public List<Dept> getDeptSch(Dept sch){
+		List<Dept> list  = new ArrayList<Dept>();
+		String sql = "SELECT * FROM DEPT01 WHERE DNAME LIKE ? AND LOC LIKE ?";
+		try( Connection con = DB.con();
+			 PreparedStatement pstmt = con.prepareStatement(sql);
+			){
+			// key word 검색에서 초기에는 데이터를 전체 출력하기 위해서 설정..
+			if(sch.getDname()==null) sch.setDname("");
+			if(sch.getLoc()==null) sch.setLoc("");
+			
+			pstmt.setString(1, "%"+sch.getDname()+"%");
+			pstmt.setString(2, "%"+sch.getLoc()+"%");
+			
+			try( ResultSet rs = pstmt.executeQuery() ){
+			
+			
+				while(rs.next()) { // 다중행일때, while 사용  
+					list.add(new Dept(rs.getInt("DEPTNO"),
+								rs.getString("DNAME"),rs.getString("LOC")));
+				}
+			}
+			
+			System.out.println("데이터 로딩 완료:"+list.size());
+		}catch(SQLException e) {
+			System.out.println("DB처리 에러:"+e.getMessage());
+		}catch(Exception e) {
+			System.out.println("기타 에러:"+e.getMessage());
+		}
+	
+		
+		
+		return list;
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		A02_EmpDao dao = new A02_EmpDao();
