84d2392cea801745503673acd4f654057a280900
양현수 <zelratole@gmail.com>
2025-05-28 19:58:02 +0900

    11

--- javaexp/database2/z02_daoExp.sql
+++ javaexp/database2/z02_daoExp.sql
@@ -27,6 +27,47 @@ public double getEmpMaxSal( String job ){
 **/
 --	4) TRY CATCH 블럭 복사
 --	5) pstmt.setXXX(1, XXX);  ?에 맞게 데이터 유형과 실제값할당.
+--	6) rs.next() : 행단위 커서 이동  하나 행 if(rs.next()) 여러행 while(rs.next()
+--	   rs.getXXX("컬럼명") : 열단위 컬럼과 타입 설정
+
+--	7) main() 결과 확인..
+-- 다중행 단일열 
+--	1) SQL 작성
+SELECT ENAME FROM EMP10 WHERE JOB = 'SALESMAN';
+--	2) SQL의 입력값과 결과값이 무엇인지 파악
+--  입력값 : JOB "SALESMAN" String job
+--  출력값 : ENAME  ALLEN WARD MARTIN TURNER  ==>    List<String> enames
+--	3) 위 내용을 기준으로 메서드 선언
+--		입력값 : 매개변수 선언
+--		결과값 : 리턴값 선언
+/*
+public List<String> getEnamesByJob( String job ){
+	List<String> enames = new ArrayList<String>();
+	String sql = "SELECT ENAME FROM EMP10 WHERE JOB = ?";
+	return enames;
+}
+List<Integer>  List<Double>  List<String>
+ * */
+SELECT SAL FROM EMP10 WHERE DEPTNO = 10;
+-- 입력 : int deptno
+-- 출력 : List<Double>
+/*
+public List<Double> getSalsByDeptno(int deptno){
+	List<Double> sals = new ArrayList<Double>();
+	String sql = "SELECT SAL FROM EMP10 WHERE DEPTNO = ?";
+	return sals;
+}	
+ * */
+ 
+
+
+
+--	4) TRY CATCH 블럭 복사
+--	5) pstmt.setXXX(1, XXX);  ?에 맞게 데이터 유형과 실제값할당.
 --	6) rs.next() : 행단위 커서 이동
 --	   rs.getXXX("컬럼명") : 열단위 컬럼과 타입 설정
 --	7) main() 결과 확인..
+
+
+
+
--- jspexp/src/main/java/jspexp/a11_database/A02_MultiAndOneDao.java
+++ jspexp/src/main/java/jspexp/a11_database/A02_MultiAndOneDao.java
@@ -1,9 +1,48 @@
 package jspexp.a11_database;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import jspexp.a10_database.DB;
+
 public class A02_MultiAndOneDao {
 
+	public List<String> getEnamesByJob( String job ){
+		List<String> enames = new ArrayList<String>();
+		String sql = "SELECT ENAME FROM EMP10 WHERE JOB = ?";
+
+
+		try( Connection con = DB.con();
+			 PreparedStatement pstmt = con.prepareStatement(sql);
+			){
+			pstmt.setString(1, job);  // 매개변수의 type을 확인해서 변수와 함께 mapping ? 
+			try( ResultSet rs = pstmt.executeQuery() ){
+				while(rs.next()) { // 다중행.. 
+					enames.add(rs.getString("ENAME"));
+				}
+			}
+			
+			System.out.println("데이터 로딩 완료:");
+		}catch(SQLException e) {
+			System.out.println("DB처리 에러:"+e.getMessage());
+		}catch(Exception e) {
+			System.out.println("기타 에러:"+e.getMessage());
+		}		
+		
+		
+		return enames;
+	}	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		A02_MultiAndOneDao dao = new A02_MultiAndOneDao();
+		for(String ename:dao.getEnamesByJob("SALESMAN")) {
+			System.out.println(ename);
+		}
 
 	}
 
