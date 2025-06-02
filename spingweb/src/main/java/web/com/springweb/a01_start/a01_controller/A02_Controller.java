29d1362056e0b48b3bbc09a2f7636ede26b22c65
양현수 <zelratole@gmail.com>
2025-06-02 14:45:22 +0900

    1111

---
+++ spingweb/src/main/java/web/com/springweb/a01_start/a01_controller/A02_Controller.java
@@ -0,0 +1,17 @@
+package web.com.springweb.a01_start.a01_controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class A02_Controller {
+	// http://locathost:5050/forth
+	// 		WEB-INF\\views\\a02_view\\a01_forth.jsp
+	@GetMapping("forth")
+	public String forthaaa() {
+		return "WEB-INF\\views\\a02_view\\a01_forth.jsp";
+	}
+}
+// A03_Controller.java
+// http://locathost:5050/five
+//  	WEB-INF\\views\\a02_view\\a02_five.jsp
