91098a7bf1857dc82b38dc21b4ce36661877021c
양현수 <zelratole@gmail.com>
2025-06-03 12:54:44 +0900

    1111

--- spingweb/src/main/java/web/com/springweb/a01_start/a01_controller/A05_RequestObjectController.java
+++ spingweb/src/main/java/web/com/springweb/a01_start/a01_controller/A05_RequestObjectController.java
@@ -3,6 +3,7 @@ package web.com.springweb.a01_start.a01_controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import web.com.springweb.vo.Person;
@@ -22,4 +23,18 @@ public class A05_RequestObjectController {
 		d.addAttribute("p01", new Person(name,age,loc));
 		return "WEB-INF\\views\\a01_begin\\a10_modelAttr.jsp";
 	}
+	// http://localhost:5050/reqOb02
+	// ModelAttribute : 요청값을 위와 받아서 모델데이터를 설정까지 한번해주는 개념..
+	//    요청값 처리 + 모델 객체 설정 처리 해주는 개념(USEBEAN개념)
+	@GetMapping("reqOb02")
+	public String reqOb01( @ModelAttribute("p01") Person p01 ) {
+		return "WEB-INF\\views\\a01_begin\\a11_modelAttr.jsp";
+	}	
+	// http://localhost:5050/reqOb03
+	/// 모델 명을 지정하지 않은 객체 소문자 시작으로 자동으로 설정 person
+	@GetMapping("reqOb03")
+	public String reqOb03(Person p01) { 
+		
+		return "WEB-INF\\views\\a01_begin\\a12_modelAttr.jsp";
+	}		
 }
