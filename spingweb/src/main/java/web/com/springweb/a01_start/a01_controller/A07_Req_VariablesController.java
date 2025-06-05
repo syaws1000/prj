028e94f9ead430a7561db51a2e36f7cf8b7ac074
양현수 <zelratole@gmail.com>
2025-06-05 15:59:44 +0900

    1111

--- spingweb/src/main/java/web/com/springweb/a01_start/a01_controller/A07_Req_VariablesController.java
+++ spingweb/src/main/java/web/com/springweb/a01_start/a01_controller/A07_Req_VariablesController.java
@@ -9,9 +9,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class A07_Req_VariablesController {
 
 	// http://localhost:5050/@@@@    해당 url에 있는 문자열로 요청값을 처리하는 경우.
-	// http://localhost:5050/himan    해당 url에 있는 문자열로 요청값을 처리하는 경우.
-	// http://localhost:5050/higirl    해당 url에 있는 문자열로 요청값을 처리하는 경우.
-	@GetMapping("/{userId}")
+	// http://localhost:5050/user/himan    해당 url에 있는 문자열로 요청값을 처리하는 경우.
+	// http://localhost:5050/user/higirl    해당 url에 있는 문자열로 요청값을 처리하는 경우.
+	@GetMapping("/user/{userId}")
 	public String getUserInf(@PathVariable String userId, Model d) {
 		// PathVariable  : 모델값을 설정
 		// view단에 대한 파일명을 부분적으로 지정할 때....
@@ -19,12 +19,19 @@ public class A07_Req_VariablesController {
 		
 		
 		d.addAttribute("userId", userId+"님!!");
-		return "WEB-INF\\views\\a01_begin\\a20_variablesExp.jsp";
+		return "/user/a20_variablesExp.jsp";
 	}
-	// http://localhost:5050/page01
-	// http://localhost:5050/page02
-	// http://localhost:5050/page03
+	// http://localhost:5050/page/page01
+	// http://localhost:5050/page/page02
+	// http://localhost:5050/page/page03
 	//   {pageNo}
-	
+	@GetMapping("/page/{pageNo}")
+	public String pageNo(@PathVariable String pageNo, Model d) {
+		// PathVariable  : 모델값을 설정
+		// view단에 대한 파일명을 부분적으로 지정할 때....
+		System.out.println("입력된:"+pageNo);
+		d.addAttribute("pageNo", "페이지번호:"+pageNo);
+		return "/page/a21_variablesExp.jsp";
+	}	
 	
 }
