332488ce19e4e0aa36f8dbcd9ffbc00109a139c7
양현수 <zelratole@gmail.com>
2025-06-09 12:50:24 +0900

    111

---
+++ spingweb/src/main/java/web/com/springweb/a01_start/a01_controller/A09_ModelAttributeController.java
@@ -0,0 +1,39 @@
+package web.com.springweb.a01_start.a01_controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import a01_diexp.vo.Person;
+
+@Controller
+public class A09_ModelAttributeController {
+	// ModelAttribute : 
+	// 개념 1 : 요청값 + 모델 데이터
+	// 개념 2 : controller 공통모델 데이터 처리..
+	@ModelAttribute("person")
+	public Person getPerson() {
+		return new Person("홍길동",25,"서울");
+	}
+	// 요청 화면 처리 
+	// http://localhost:5050/show10
+	@GetMapping("show10")
+	public String show10() {
+		return "WEB-INF\\views\\a02_view\\a10_show.jsp";
+	}
+	// http://localhost:5050/show11
+	@GetMapping("show11")
+	public String show11() {
+		return "WEB-INF\\views\\a02_view\\a11_show.jsp";
+	}
+	// http://localhost:5050/show12
+	@GetMapping("show12")
+	public String show12() {
+		return "WEB-INF\\views\\a02_view\\a11_show.jsp";
+	}
+	
+	
+	
+	
+	
+}
