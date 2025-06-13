d0a10cb55d821dc2e43718167ece206093f2bb04
양현수 <zelratole@gmail.com>
2025-06-13 16:10:48 +0900

    111

--- spingweb/src/main/java/web/com/springweb/a04_mvc/a01_controller/A02_PurchaseController.java
+++ spingweb/src/main/java/web/com/springweb/a04_mvc/a01_controller/A02_PurchaseController.java
@@ -1,8 +1,23 @@
 package web.com.springweb.a04_mvc.a01_controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import web.com.springweb.a04_mvc.a02_service.A02_PurchaseService;
+import web.com.springweb.a04_mvc.a04_vo.Purchase;
 
 @Controller
 public class A02_PurchaseController {
+	@Autowired(required=false)
+	private A02_PurchaseService service;
+	
+	// http://localhost:5050/purchaseList
+	@RequestMapping("purchaseList")
+	public String purchaseList(Purchase sch, Model d) {
+		d.addAttribute("list", service.getPurchaseList(sch));
+		return "WEB-INF\\views\\a04_mvc\\a04_purchaseList.jsp";
+	}
 
 }
