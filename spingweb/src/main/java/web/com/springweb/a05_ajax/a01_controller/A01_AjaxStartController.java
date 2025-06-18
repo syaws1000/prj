ce2415d2db31d4c48a46c848edc55cc008cb78d4
양현수 <zelratole@gmail.com>
2025-06-18 17:10:54 +0900

    1111

---
+++ spingweb/src/main/java/web/com/springweb/a05_ajax/a01_controller/A01_AjaxStartController.java
@@ -0,0 +1,14 @@
+package web.com.springweb.a05_ajax.a01_controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class A01_AjaxStartController {
+	// html view 호출..
+	// http://localhost:5050/showFront
+	@GetMapping("showFront")
+	public String showFront() {
+		return "a01_show.html";
+	}
+}
