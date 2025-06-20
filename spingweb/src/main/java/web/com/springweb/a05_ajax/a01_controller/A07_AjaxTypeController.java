c762a6ca08206099d84575d5664f2a975e93a216
양현수 <zelratole@gmail.com>
2025-06-20 11:59:36 +0900

    1111

---
+++ spingweb/src/main/java/web/com/springweb/a05_ajax/a01_controller/A07_AjaxTypeController.java
@@ -0,0 +1,9 @@
+package web.com.springweb.a05_ajax.a01_controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class A07_AjaxTypeController {
+	// 요청 type에 대한 처리
+
+}
