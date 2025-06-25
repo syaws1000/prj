4f99c34a5c33d30fe334099a008b55523b7e6ae6
양현수 <zelratole@gmail.com>
2025-06-25 14:55:06 +0900

    111

--- spingweb/src/main/java/web/com/springweb/a06_board/A01_BoardController.java
+++ spingweb/src/main/java/web/com/springweb/a06_board/A01_BoardController.java
@@ -2,6 +2,7 @@ package web.com.springweb.a06_board;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -11,7 +12,10 @@ public class A01_BoardController {
 	
 	// http://localhost:5050/boardList
 	@GetMapping("boardList")
-	public String boardList() {
+	public String boardList(BoardSch sch, Model d) {
+		
+		d.addAttribute("blist", service.getBoardList(sch));
+		
 		return "WEB-INF\\views\\a05_board\\a01_boardList.jsp";
 	}
 }
--- spingweb/src/main/java/web/com/springweb/a06_board/A02_BoardService.java
+++ spingweb/src/main/java/web/com/springweb/a06_board/A02_BoardService.java
@@ -1,5 +1,7 @@
 package web.com.springweb.a06_board;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -7,4 +9,14 @@ import org.springframework.stereotype.Service;
 public class A02_BoardService {
 	@Autowired(required=false)
 	private A03_BoardDao dao;
+	
+	public List<Board> getBoardList(BoardSch sch){
+		if(sch.getSubject()==null) sch.setSubject("");
+		if(sch.getWriter()==null) sch.setWriter("");
+		
+		sch.setSubject("%"+sch.getSubject()+"%");
+		sch.setWriter("%"+sch.getWriter()+"%");
+		
+		return dao.getBoardList(sch);
+	}
 }
