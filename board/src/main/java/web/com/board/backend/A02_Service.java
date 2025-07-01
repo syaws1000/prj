07f6bd278c33f6b838e9d78b1fcddaab54d1de82
양현수 <zelratole@gmail.com>
2025-07-01 09:58:15 +0900

    111

--- board/src/main/java/web/com/board/backend/A02_Service.java
+++ board/src/main/java/web/com/board/backend/A02_Service.java
@@ -14,6 +14,10 @@ public class A02_Service {
 	@Autowired(required = false)
 	private A03_Dao dao;
 	
+	public Member login(Member sch) {
+		return dao.login(sch);
+	}
+	
 	public List<Announce> getAnnounceList(AnnounceSch sch){
 		if(sch.getTitle()==null) sch.setTitle("");
 		if(sch.getAuthor()==null) sch.setAuthor("");
