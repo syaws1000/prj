4373a14bd9f9c3533792376bce614b3b38929c0a
양현수 <zelratole@gmail.com>
2025-07-02 16:37:15 +0900

    111

--- spingweb/src/main/java/web/com/springweb/a04_mvc/a01_controller/A07_FileUploadController.java
+++ spingweb/src/main/java/web/com/springweb/a04_mvc/a01_controller/A07_FileUploadController.java
@@ -1,5 +1,6 @@
 package web.com.springweb.a04_mvc.a01_controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,9 +8,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import web.com.springweb.a04_mvc.a02_service.A07_FileUploadService;
+
 @Controller
 public class A07_FileUploadController {
 
+	@Autowired(required=false)
+	private A07_FileUploadService service;
+	
 	// http://localhost:5050/upload102
 	@GetMapping("upload102")
 	public String upload102() {
@@ -18,7 +24,7 @@ public class A07_FileUploadController {
 	@PostMapping("upload102")
 	public String upload103(@RequestParam("report") MultipartFile mf, Model d) {
 		System.out.println("업로드 파일:"+mf.getOriginalFilename());
-		d.addAttribute("msg", "업로드 성공");
+		d.addAttribute("msg", service.upload4(mf));
 		return "WEB-INF\\views\\a04_mvc\\a12_fileUploadExp.jsp";
 	}
 	
