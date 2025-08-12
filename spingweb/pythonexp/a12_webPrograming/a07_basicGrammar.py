762c93dc5899f673c42a764418eb9aa0e22e0a26
양현수 <zelratole@gmail.com>
2025-08-12 11:46:53 +0900

    1111

---
+++ spingweb/pythonexp/a12_webPrograming/a07_basicGrammar.py
@@ -0,0 +1,12 @@
+# 1. 모들 import
+from flask import Flask, render_template, request, redirect
+# 2. 객체 생성
+app = Flask(__name__, static_url_path="") # static 폴드 지정..
+
+# 3. template 안에 특정 html 파일로 호출..
+# http://localhost:8888/start
+@app.route("/start")
+def start():
+    return render_template("a01_basic/a06_templateBasic.html") # templates 폴드하에 특정 페이지 호출..
+app.run(port=8888, debug=True)
+
